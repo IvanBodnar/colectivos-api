7ec6e4f66cef479355ac552a614e61839e44b84e
ivanbodnar <ibodnar@outlook.com>
2019-06-22 16:22:32 -0300

    Stops model created

--- app/stops/models.py
+++ app/stops/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Stops(models.Model):
+    tipo = models.CharField(max_length=255, null=True)
+    calle = models.CharField(max_length=255, null=True)
+    numero = models.IntegerField(null=True)
+    entre1 = models.CharField(max_length=255, null=True)
+    entre2 = models.CharField(max_length=255, null=True)
+    lineas = ArrayField(base_field=models.IntegerField())
+    dir_norm = models.CharField(max_length=255, null=True)
+    metrobus = models.BooleanField()
+    stop_desc = models.CharField(max_length=255, null=True)
+    x = models.FloatField()
+    y = models.FloatField()
+    geom = models.PointField(null=True)
+    geom_98334 = models.PointField(srid=98334, null=True)
+
+    class Meta:
+        db_table = 'stops'
+
+    def __repr__(self):
+        return f'Stop id: {self.id}'
