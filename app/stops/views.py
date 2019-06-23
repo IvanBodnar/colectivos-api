36d17533995394fc5a5e6e1707ef312778296869
ivanbodnar <ibodnar@outlook.com>
2019-06-23 19:38:36 -0300

    Method get_queryset overriden on StopListView to filter by radius if queryparams are sent

--- app/stops/views.py
+++ app/stops/views.py
@@ -5,5 +5,13 @@ from .serializers import StopSerializer
 
 
 class StopListView(ListAPIView):
-    queryset = Stop.objects.all()
     serializer_class = StopSerializer
+
+    def get_queryset(self):
+        queryset = Stop.objects.all()
+        x = self.request.query_params.get('x', None)
+        y = self.request.query_params.get('y', None)
+        radius = self.request.query_params.get('radius', None)
+        if all((x, y, radius)):
+            queryset = Stop.objects.radius_filter(float(x), float(y), float(radius))
+        return queryset
