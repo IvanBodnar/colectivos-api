da1d1d5a9c5a7bdf645c03478e30a1984dd4def6
ivanbodnar <ibodnar@outlook.com>
2019-06-22 22:36:11 -0300

    StopSerializer created

---
+++ app/stops/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from .models import Stop
+
+
+class StopSerializer(serializers.ModelSerializer):
+    location = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Stop
+        fields = ('id', 'tipo', 'location', 'lineas')
+        read_only_fields = ('id',)
