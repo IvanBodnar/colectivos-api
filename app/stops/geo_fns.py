de31f4df14a4029c7d8f3ecb539aa9cb16fbf230
ivanbodnar <ibodnar@outlook.com>
2019-06-23 14:36:11 -0300

    geo_fns.py added -> point_4326_to_98334 fn

---
+++ app/stops/geo_fns.py
@@ -0,0 +1,17 @@
+from django.contrib.gis.geos import Point
+
+
+# The proj4 definition must be added because
+# django doesn't seem to be able to pick it up
+# from spatial_ref_sys.
+proj4 = '''
+    +proj=tmerc +lat_0=-34.6297166 +lon_0=-58.4627 +k=1
+    +x_0=100000 +y_0=100000 +ellps=intl +units=m
+    +towgs84=-148,136,90,0,0,0,0 +no_defs
+'''
+
+
+def point_4326_to_98334(x: float, y: float) -> Point:
+    point = Point(x, y, srid=4326)
+    point.transform(proj4)
+    return point
